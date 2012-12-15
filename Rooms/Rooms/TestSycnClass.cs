cbe79ccca1786eede74c674a2a3f6af15d471f23
HecnTV <MahanovR@gmail.com>
2012-12-16 00:39:55 +0600

    Add TestSync class

---
+++ Rooms/Rooms/TestSycnClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Rooms
+{
+    public class TestSycnClass
+    {
+        private int b;
+        public int _b
+        {
+            get { return b; } 
+            set { b = value; }
+        }
+    }
+}
